1ff1031de131b9baad9226653dafc54c3f21843a
protimsaha <protimsaha750@gmail.com>
2022-05-04 23:28:55 +0600

    delete methode add

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ MongoClient.connect(uri, function (err, client) {
                 res.send(result)
             })
 
+            app.delete('/bikes/:id', async (req, res) => {
+                const id = req.params.id;
+                const query = { _id: ObjectId(id) }
+                const result = await bikeCollection.deleteOne(query)
+                res.send(result)
+            })
+
         }
         finally {
 
