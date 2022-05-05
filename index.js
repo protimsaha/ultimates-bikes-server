aac067397e16b80732ef36f82d4e2f962bb7b3f1
protimsaha <protimsaha750@gmail.com>
2022-05-06 00:53:41 +0600

    log in jwt implemented

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
+// const verify = require('jsonwebtoken/verify');
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
-
-// var MongoClient = require('mongodb').MongoClient;
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorize User' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.e3npc.mongodb.net:27017,cluster0-shard-00-01.e3npc.mongodb.net:27017,cluster0-shard-00-02.e3npc.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-rgtnsu-shard-0&authSource=admin&retryWrites=true&w=majority`;
 MongoClient.connect(uri, function (err, client) {
@@ -18,6 +32,15 @@ MongoClient.connect(uri, function (err, client) {
             await client.connect()
             const bikeCollection = client.db('assignment11').collection('bikes')
             const itemCollection = client.db('assignment11').collection('item')
+
+            // AUTH
+            app.post('/login', async (req, res) => {
+                const user = req.body
+                const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5d' })
+                res.send({ accessToken })
+            })
+
+
 
             // get all item
             app.get('/bikes', async (req, res) => {
@@ -51,12 +74,17 @@ MongoClient.connect(uri, function (err, client) {
             })
 
             // get items by specific user
-            app.get('/items', async (req, res) => {
+            app.get('/items', verifyJWT, async (req, res) => {
+                const decodedEmail = req.decoded?.email;
                 const email = req.query.email;
-                const query = { email: email }
-                const cursor = itemCollection.find(query)
-                const items = await cursor.toArray()
-                res.send(items)
+                if (email === decodedEmail) {
+                    const query = { email: email }
+                    const cursor = itemCollection.find(query)
+                    const items = await cursor.toArray()
+                    res.send(items)
+                } else {
+                    res.status(403).send({ message: 'Forbidden access' })
+                }
             })
 
             app.delete('/items/:id', async (req, res) => {
